5519b138f0e01530177ec4a7368c5c6ba98704a2
mjyousif <33187058+mjyousif@users.noreply.github.com>
2023-07-21 22:29:55 -0400

    venmo url fix

--- src/utilities/venmo.tsx
+++ src/utilities/venmo.tsx
@@ -1,10 +1,13 @@
 import { isMobile } from "react-device-detect";
 
-function generateVenmoUrl(amount: string, note: string = ""): string {
+function generateVenmoUrl(
+  amount: string,
+  note: string = "Quick-split request"
+): string {
   if (isMobile) {
-    return `https://venmo.com/?txn=charge&note=${note}&amount=${amount}`;
+    return `https://venmo.com/?txn=charge&amount=${amount}&note=${note}`;
   } else {
-    return `https://account.venmo.com/pay?amount=${amount}&note=note=${note}`;
+    return `https://account.venmo.com/pay?amount=${amount}&note=${note}`;
   }
 }
 
